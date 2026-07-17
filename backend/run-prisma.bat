5dd4e1ad49c02febcd3daac7e02b14ffc42a78c7
Zakaria Chamekh <chamekhzakaria95@gmail.com>
2026-07-17 21:17:37 +0100

    Revert "save partie 1 & 2"

--- backend/run-prisma.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-npx prisma migrate deploy
-npx prisma generate
--- frontend/src/pages/DealsKanbanPage.tsx
+++
@@ -1,9 +0,0 @@
-import DealsKanban from '../components/deals/DealsKanban'
-
-export default function DealsKanbanPage() {
-  return (
-    <div className="h-[calc(100vh-64px)]">
-      <DealsKanban />
-    </div>
-  )
-}
